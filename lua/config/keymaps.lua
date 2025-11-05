5f70a08b6000ad8d1c24bd039fd9c0b29ad7f2dd
Pansther <werawit_boat@windowslive.com>
2025-11-05 09:40:15 +0700

    feat: Add floating error

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,11 @@ vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit Insert Mode" })
 vim.keymap.set("i", "kj", "<Esc>", { desc = "Exit Insert Mode" })
 vim.keymap.set("i", "<C-z>", "<C-o>u", { desc = "Undo in insert mode" })
 vim.keymap.set("i", "<C-y>", "<C-o><C-r>", { desc = "Redo in insert mode" })
+
+vim.keymap.set("n", "K", function()
+  if vim.lsp.get_clients() then
+    vim.lsp.buf.hover()
+  else
+    vim.diagnostic.open_float({})
+  end
+end, { desc = "LSP/Diagnostic Hover/Float" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,3 +13,24 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
   end,
 })
+
+vim.diagnostic.config({
+  float = {
+    source = "always", -- แสดงชื่อ source (เช่น 'eslint', 'tsserver') เสมอ
+    border = "rounded", -- รูปแบบของขอบ: 'single', 'double', 'rounded'
+    focusable = false, -- ไม่ให้โฟกัสที่ floating window
+    header = "", -- ข้อความ Header
+  },
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    -- เปิด floating window แสดง diagnostic ที่ตำแหน่งเคอร์เซอร์
+    vim.diagnostic.open_float({
+      -- ให้แสดงเฉพาะ diagnostic ของบรรทัดปัจจุบัน
+      scope = "cursor",
+    })
+  end,
+  -- ให้ทำงานกับ buffer ที่เปิดใช้งาน LSP
+  group = vim.api.nvim_create_augroup("DiagnosticFloat", { clear = true }),
+})
