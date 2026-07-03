4c4466f5a715c7dd8f52ed0b9e093ad01fe73bed
Pansther <werawit_boat@windowslive.com>
2026-07-03 16:57:53 +0700

    feat: Add astro

---
+++ lua/plugins/astro.lua
@@ -0,0 +1,5 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.typescript" }, -- Or your respective framework pack
+  { import = "astrocommunity.formatter.conform-nvim" },
+}
