f16401442eaa6f255e987567263a0b1262472c7e
Pansther <werawit_boat@windowslive.com>
2026-08-03 14:54:24 +0700

    feat: Remove astro

--- lua/plugins/astro.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.typescript" }, -- Or your respective framework pack
-  { import = "astrocommunity.formatter.conform-nvim" },
-}
